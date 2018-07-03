7560d8d991b804c80b11969b4e13e99215caae86
alcarriaIBM <34882458+alcarriaIBM@users.noreply.github.com>
2018-07-03 12:13:29 +0200

    Update virtualserver.py

--- virtualserver.py
+++ virtualserver.py
@@ -47,6 +47,32 @@ client = SoftLayer.create_client_from_env(
 )
 productOrderService = client['SoftLayer_Product_Order']
 
+order = {
+    'complexType': 'SoftLayer_Container_Product_Order_Virtual_Guest',
+    'quantity': 1,
+    'virtualGuests': [
+        {'hostname': 'test-template', 'domain': 'example.com'}
+    ],
+    'location': 168642,  # San Jose 1
+    'packageId': 46,  # CCI Package
+    'prices': [
+        {'id': 1640},  # 1 x 2.0 GHz Core
+        {'id': 1644},  # 1 GB RAM
+        {'id':  905},  # Reboot / Remote Console
+        {'id':  272},  # 10 Mbps Public & Private Networks
+        {'id':50231},  # 1000 GB Bandwidth
+        {'id':   21},  # 1 IP Address
+        {'id': 2202},  # 25 GB (SAN)
+        {'id': 1684},  # CentOS 5 - Minimal Install (32 bit)
+        {'id':   55},  # Host Ping Monitoring
+        {'id':   57},  # Email and Ticket Notifications
+        {'id':   58},  # Automated Notification Response
+        {'id':  420},  # Unlimited SSL VPN Users & 1 PPTP VPN User per account
+        {'id':  418},  # Nessus Vulnerability Assessment & Reporting
+    ],
+    'imageTemplateId': templateId
+}
+
 
 """
 To test the input parameters call the SoftLayer_Virtual_Guest::generateOrderTemplate method
@@ -54,9 +80,9 @@ when you are ready to create the server call the createObject method instead.
 """
 try:
 
-    newOrder = client['Virtual_Guest'].generateOrderTemplate(orderToRequest)
-    pp(newOrder)
-    # response = productOrderService.verifyOrder(orderData)
+    # newOrder = client['Virtual_Guest'].generateOrderTemplate(order)
+    # pp(newOrder)
+    # response = productOrderService.verifyOrder(order)
 
 
 except SoftLayer.SoftLayerAPIError as e:
